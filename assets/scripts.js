73f10c6e039fd3c9e145ed511beb040ac62f307b
Afonso de Mori A. Silva <afonso@demori.xyz>
2019-02-16 19:00:15 +0100

    Fix data fetched from API, fix color coherence, add data columns

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,13 +8,14 @@ function init(keys) {
             '<td class="align-middle latest">' +
             '<span class="reason">loading...</span>' +
             '<span class="time">loading...</span>' +
-            '<span class="duration">loading...</span></td>' +
-            '<td class="align-middle text-center"><span class="r0 ratio-color-loading">loading...</span></td>' +
-            '<td class="align-middle text-center"><span class="r1 ratio-color-loading">loading...</span></td>' +
-            '<td class="align-middle text-center"><span class="r2 ratio-color-loading">loading...</span></td>' +
-            '<td class="align-middle text-center"><span class="r3 ratio-color-loading">loading...</span></td>' +
-            '</tr>'
+            '<span class="duration">loading...</span></td>'
         ;
+
+        for (let r = 0; r < 9; r++) {
+            append += '<td class="r-' + r + '"><span class="r' + r + ' ratio-color-loading">loading...</span></td>';
+        }
+
+        append += '</tr>';
     }
 
     $('table').append(append);
@@ -62,14 +63,30 @@ function formatDuration(duration) {
 }
 
 function loadData() {
+    let now = new Date();
+
+    let last24h = (Math.floor(now.getTime() / 1000) - 60 * 60 * 24) + '_' + Math.floor(now.getTime() / 1000);
+    let last30d = (Math.floor(now.getTime() / 1000) - 60 * 60 * 24 * 30) + '_' + Math.floor(now.getTime() / 1000);
+
+    let ranges = last24h + '-' + last30d;
+
+    let day = now;
+    day.setHours(0, 0, 0, 0);
+    let range = [];
+    for (let i = 0; i < 7; i++) {
+        $('.d-' + i).html(day.getDate() + '/' + (day.getMonth() + 1));
+        range[0] = Math.floor(day.getTime() / 1000);
+        range[1] = Math.floor(day.setDate(day.getDate() - 1) / 1000);
+        ranges += '-' + range[1] + '_' + range[0];
+    }
+
     for (let m = 0; m < apiKeys.length; m++) {
         $.ajax({
             type: 'post',
             url: 'https://api.uptimerobot.com/v2/getMonitors',
             data: {
                 api_key: apiKeys[m].key,
-                custom_uptime_ratios: '1-7-15-30',
-                // custom_uptime_ranges: '1465440758_1466304758',
+                custom_uptime_ranges: ranges,
                 logs: 1,
                 logs_limit: 1
             },
@@ -77,7 +94,7 @@ function loadData() {
             success: function (data) {
                 let monitor = data.monitors[0];
                 let id = '#monitor-' + monitor.id;
-                let ratio = monitor.custom_uptime_ratio.split('-');
+                let ratio = monitor.custom_uptime_ranges.split('-');
 
                 $(id + ' .status')
                     .removeAttr('class')
@@ -98,11 +115,21 @@ function loadData() {
 
                 let color = 'inherit';
                 switch (monitor.status) {
-                    case 0: color = 'var(--monitor-paused)'; break;
-                    case 1: color = 'var(--monitor-not-checked-yet)'; break;
-                    case 2: color = 'var(--monitor-up)'; break;
-                    case 8: color = 'var(--monitor-seems-offline)'; break;
-                    case 9: color = 'var(--monitor-down)'; break;
+                    case 0:
+                        color = 'var(--monitor-paused)';
+                        break;
+                    case 1:
+                        color = 'var(--monitor-not-checked-yet)';
+                        break;
+                    case 2:
+                        color = 'var(--monitor-up)';
+                        break;
+                    case 8:
+                        color = 'var(--monitor-seems-offline)';
+                        break;
+                    case 9:
+                        color = 'var(--monitor-down)';
+                        break;
                 }
 
                 $(id + ' .latest .reason').css('color', color);
